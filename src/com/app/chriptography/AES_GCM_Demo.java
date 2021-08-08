09de7160b4f1acfcce535e86b4d164a7e23c6d10
Karunakar Thurlu <karunakarthurlujava@gmail.com>
2021-08-08 20:40:54 +0530

    08-08-2021

--- src/com/app/chriptography/AES_GCM_Demo.java
+++ src/com/app/chriptography/AES_GCM_Demo.java
@@ -1,43 +1,43 @@
 package com.app.chriptography;
 
+import javax.crypto.*;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
+import java.security.spec.KeySpec;
 import java.util.Base64;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.KeyGenerator;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.GCMParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
 
 
 
 public class AES_GCM_Demo {
- 
 
+	private static final String ALGORITHAM ="AES";
+	private static final String AES_ALGORITHAM_MODE ="AES/GCM/NoPadding";
 	private static final int AES_KEY_SIZE = 256;
 	private static final int GCM_IV_LENGTH = 12;
 	private static final int GCM_TAG_LENGTH = 16;
+	private static final int SALT_LENGTH_BYTE = 16;
 	private byte[] IV = new byte[GCM_IV_LENGTH];
+	private static byte[] SALT = new byte[SALT_LENGTH_BYTE];
+	private static final Charset UTF_8 = StandardCharsets.UTF_8;
 
 	private KeyGenerator keyGenerator;
 	private SecretKey secretKey;
 	private Cipher ecipher;
 	private Cipher dcipher;
 
-	public AES_GCM_Demo()  {
+	public AES_GCM_Demo(String key)  {
 		try {
-			keyGenerator=KeyGenerator.getInstance("AES");
-			keyGenerator.init(AES_KEY_SIZE);
-			secretKey=keyGenerator.generateKey();
-			new SecureRandom().nextBytes(IV);
-			ecipher=Cipher.getInstance("AES/GCM/NoPadding");
-			dcipher=Cipher.getInstance("AES/GCM/NoPadding");
+			this.secretKey=generateKey(key.toCharArray());
+			this.ecipher=Cipher.getInstance(AES_ALGORITHAM_MODE);
+			this.dcipher=Cipher.getInstance(AES_ALGORITHAM_MODE);
+			this.ecipher.init(Cipher.ENCRYPT_MODE, secretKey, new GCMParameterSpec(GCM_TAG_LENGTH * 8, IV));
+			this.dcipher.init(Cipher.DECRYPT_MODE,secretKey, new GCMParameterSpec(GCM_TAG_LENGTH * 8, IV));
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
 		}catch(NoSuchPaddingException e) {
@@ -46,41 +46,35 @@ public class AES_GCM_Demo {
 			e.printStackTrace();
 		}
 	}
-	public byte[] doEncrypt(String rawString) {
-		SecretKeySpec keySpec=new SecretKeySpec(secretKey.getEncoded(), "AES");
-		GCMParameterSpec gcmParameterSpec =new GCMParameterSpec(GCM_TAG_LENGTH * 8, IV);
+
+	public static SecretKey generateKey(char[] password) throws  Exception {
+		SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+		KeySpec spec = new PBEKeySpec(password, SALT, 65536, 256);
+		SecretKey secret = new SecretKeySpec(factory.generateSecret(spec).getEncoded(), ALGORITHAM);
+		return secret;
+	}
+	public String doEncrypt(String rawString) {
 		try {
-			ecipher.init(Cipher.ENCRYPT_MODE, keySpec, gcmParameterSpec);
 			byte[] cipherText= ecipher.doFinal(rawString.getBytes());
-			return cipherText;
-		}catch (InvalidKeyException e) {
-			e.printStackTrace();
-		} catch (InvalidAlgorithmParameterException e) {
-			e.printStackTrace();
-		}catch (IllegalBlockSizeException e) {
-			e.printStackTrace();
-		} catch (BadPaddingException e) {
-			e.printStackTrace();
+			return Base64.getEncoder().encodeToString(cipherText);
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
 
-	public String doDecrypt(byte[] decryptedString) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException  {
-		SecretKeySpec keySpec = new SecretKeySpec(secretKey.getEncoded(), "AES");
-		GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(GCM_TAG_LENGTH * 8, IV);
-		dcipher.init(Cipher.DECRYPT_MODE, keySpec, gcmParameterSpec);
-		byte[] decryptedText = dcipher.doFinal(decryptedString);
-		return new String(decryptedText);
+	public String doDecrypt(String decryptedString) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException  {
+		byte[] decode = Base64.getDecoder().decode(decryptedString.getBytes(UTF_8));
+		byte[] decryptedText = dcipher.doFinal(decode);
+		return new String(decryptedText, UTF_8);
 	}
 
 	public static void main(String[] args) throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException {
-		AES_GCM_Demo aes=new AES_GCM_Demo();
-		String plainText = "Karunakar Thurlu";
+		AES_GCM_Demo aes=new AES_GCM_Demo("D!gT@lBuY");
+		String plainText = "Karunakar_Thurlu";
 		System.out.println("Original Text : " + plainText);
-		byte[] cipherText = aes.doEncrypt(plainText);
-		System.out.println("Encrypted Text : " + Base64.getEncoder().encodeToString(cipherText));
+		String cipherText = aes.doEncrypt(plainText);
+		System.out.println("Encrypted Text : " + cipherText);
 
 		String decryptedText = aes.doDecrypt(cipherText);
 		System.out.println("DeCrypted Text : " + decryptedText);
